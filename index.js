4fa8f24848418b7aa1a532f4fadc67beb85a6ead
qaed4 <sanek-nagovicin@list.ru>
2022-10-21 00:34:39 +0300

    check repo

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 import half from './src/half.js';
 
 export default half;
-
-document.querySelector('.b1').addEventListener('click', () => {
-    let data = document.querySelector('.i1').nodeValue;
-    document.querySelector('.out1').innerHTML  = data;
-})
+//
+// document.querySelector('.b1').addEventListener('click', () => {
+//     let data = document.querySelector('.i1').nodeValue;
+//     document.querySelector('.out1').innerHTML  = data;
+// })
